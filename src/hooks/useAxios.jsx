0be3837da14354e3387ff51deddbde77e2fa2d4c
danx <rushn24@gmail.com>
2024-08-17 22:49:54 -0400

    experimenting with useAxios hook

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,26 @@
-import React, { usestate } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const useAxios = (url) => {
+    const [res, setRes] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await axios.get(url)
+                setRes(res)
+                console.log(res)
+            } catch(e) {
+                setError(e)
+            };
+            setIsLoading(false)
+        };
+        getData()
+    }, [url]);
+
+    return { res, error, isLoading }
 };
 
 export default useAxios;
